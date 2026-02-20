645bae404d6a2c207f51c53d7fa9f1ef1b7979dc
Mahendar-77 <mahendarlokini@gmail.com>
2026-02-20 17:27:57 +0530

    Updated_admin

--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://192.168.88.19:5000",
+  baseURL: "http://192.168.88.15:5000",
+  // baseURL: "http://localhost:5000",
 });
 
 axiosInstance.interceptors.request.use((config) => {
